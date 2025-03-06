14e3133962d6b547d3ee58a96309900e733dae8e
davidinovargas <davidinovargas@gmail.com>
2025-03-06 17:07:34 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double resultado = 0;
+
+            Console.WriteLine("Calculadora Tabajara 2025");
+
+            Console.Write("Digite o primeiro número: ");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine("Primeiro Número: " + primeiroNumero);
+            Console.WriteLine("segundo Número: " + segundoNumero);
+            Console.WriteLine("Resultado " + resultado);
+
+            Console.ReadLine();
+
+        }
+    }
+}
