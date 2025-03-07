93048d01f01eb50df56c65e85c1ef9947efee4a0
davidinovargas <davidinovargas@gmail.com>
2025-03-07 16:23:12 -0300

    Adicionado funcionalidade de subtração, divisão, laço para continuar na aplicação e opção para realizar operações com o resultado. (mantendo a estrutura simples dos códigos anteriores)

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -1,54 +1,93 @@
-﻿namespace Calculadora.ConsoleApp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculadora.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double resultado = 0;
+            bool continuarOperando = false;
 
             Console.WriteLine("______________________________");
             Console.WriteLine("Calculadora Tabajara 2025");
             Console.WriteLine("______________________________");
 
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("S - Sair");
-
-            Console.WriteLine();
-            Console.Write("Escolha uma opção: ");
-
-            //estrutura de decisão
-            
-            string opcao = Console.ReadLine().ToUpper();
-            if (opcao == "S")
+            while (true)
             {
-                return;
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicar");
+                Console.WriteLine("4 - Dividir");
+                Console.WriteLine("S - Sair");
+                Console.WriteLine();
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao == "S")
+                {
+                    return;
+                }
+
+                double primeiroNumero;
+                if (continuarOperando)
+                {
+                    primeiroNumero = resultado;
+                    Console.WriteLine("resultado anterior: " + primeiroNumero);
+                }
+                else
+                {
+                    Console.Write("Digite o primeiro número: ");
+                    primeiroNumero = Convert.ToDouble(Console.ReadLine());
+                }
+
+                Console.Write("Digite o segundo número: ");
+                double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                bool valido = true;
+
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+                else if (opcao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+                else if (opcao == "3")
+                {
+                    resultado = primeiroNumero * segundoNumero;
+                }
+                else if (opcao == "4")
+                {
+                    if (segundoNumero != 0)
+                    {
+                        resultado = primeiroNumero / segundoNumero;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Inválido: divisão por zero.");
+                        valido = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida.");
+                    valido = false;
+                }
+
+                if (valido)
+                {
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+
+                Console.WriteLine("Continuar usando o resultado? (s/n)");
+                char continuar = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                continuarOperando = char.ToLower(continuar) == 's';
             }
-
-            Console.Write("Digite o primeiro número: ");
-            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite o segundo número: ");
-            double segundoNumero = Convert.ToDouble(Console.ReadLine());
-
-            double resultado = 0;
-
-            if (opcao == "1") {
-
-                resultado = primeiroNumero + segundoNumero;
-
-            }
-
-            else {
-
-                resultado = primeiroNumero - segundoNumero;
-            }
-
-            Console.WriteLine("Primeiro Número: " + primeiroNumero);
-            Console.WriteLine("segundo Número: " + segundoNumero);
-            Console.WriteLine("Resultado " + resultado);
-
-            Console.ReadLine();
-
         }
     }
 }
